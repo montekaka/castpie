dcb89ec7dbacaafbd1fefd3a1d4ec48dc043db7e
montekaka <josh.chen.jia@gmail.com>
2018-12-23 10:52:49 -0800

    configure the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,16 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'env']
-       }
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { 
+          presets: ['@babel/preset-env', '@babel/preset-react'] 
+        }
       }
     ]
-  }
+  },
+  resolve: { extensions: ['*', '.js', ',jsx'] },
 };
