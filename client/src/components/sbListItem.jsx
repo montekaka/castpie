61941967383d93a8fea9880d03c7549be61ee680
montekaka <josh.chen.jia@gmail.com>
2018-12-30 18:22:16 -0800

    add condition rendering to handle download button

--- client/src/components/sbListItem.jsx
+++ client/src/components/sbListItem.jsx
@@ -8,10 +8,12 @@ class SbListItem extends React.Component {
     super(props);
     this.state = {
       title: '',
-      text: ''      
+      text: '',
+      downloadUrl: ''
     }
 
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.post = this.post.bind(this);
   }
 
   componentDidMount() {
@@ -21,19 +23,36 @@ class SbListItem extends React.Component {
     })
   }
 
-  handleClick(){
-    // console.log(this.state.rss)
+  handleClick() {
+    // 1. open the modal, telling user we are working to convert the article to audio
+    // 2. make a post request
+    // 3. close the modal, and download the file
+    this.props.modalDownloadClickedToggle();
+    this.post();
+  }
+
+  post() {
     const data = {title: this.state.title, text: this.state.text, outputFormat: 'mp3', voiceId: 'Kimberly'};
     axios.post('/api/article', data)
-    .then((res) => {
-      //console.log(res)
-      window.open(res.data);
+    .then((res) => {      
+      this.setState({downloadUrl: res.data}, () => {
+        this.props.modalDownloadClickedToggle();
+      });
     }).catch((err) => {
       console.log(err)
-    })
+    })    
   }
-  
+  //<a href="path_to_file" download="proposed_file_name">Download</a>
+
   render() {
+
+    let downloadButton;
+    if (this.state.downloadUrl.length > 0) {
+      downloadButton = <a className="btn btn-primary btn-sm" href={this.state.downloadUrl}>Ready to download</a>
+    } else {
+      downloadButton = <button className="btn btn-outline-primary btn-sm" onClick={this.handleClick}>convert the article</button>
+    }
+
     return(
       <li className="media list-group-item p-4">
         <div className="media-body">
@@ -41,7 +60,7 @@ class SbListItem extends React.Component {
             <small className="float-right text-muted">{this.props.item.pubDate}</small>
             <h6>{this.state.title}</h6>
           </div>
-          <button className="btn btn-outline-primary btn-sm" onClick={this.handleClick}>Download and listen full article</button>
+          {downloadButton}          
           <SbListItemParagraph text={this.state.text}/>          
           <SbListItemImages text={this.state.text}/>          
         </div>        
